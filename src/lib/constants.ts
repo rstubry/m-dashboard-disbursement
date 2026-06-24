d47fff531a0e3f59aa15cf9472a79844cd3749e6
rstubryan <restubumirr@gmail.com>
2026-06-24 09:15:04 +0700

    chore(FE-constant): Add BANKS constant with supported banks

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+import type { Bank } from "@/models/transaction";
+
+export const BANKS: Bank[] = [
+  "BCA",
+  "BRI",
+  "BNI",
+  "Mandiri",
+  "BSI",
+  "CIMB Niaga",
+  "Permata",
+  "Danamon",
+  "BTN",
+];
